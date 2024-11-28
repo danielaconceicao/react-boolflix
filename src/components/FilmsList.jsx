b29a0fda182c8d308c7da60e84ac938efde369d6
danielaconceicao <daniela.3597@gmail.com>
2024-11-28 17:03:26 +0100

    milestone 1

--- src/components/FilmsList.jsx
+++ src/components/FilmsList.jsx
@@ -1,8 +1,16 @@
-import { useContext } from "react" 
+import { useContext, useState, useEffect } from "react" 
 import ApiContext from "../context/ApiContext"
 
 export default function LayoutPage(){
     const { films } = useContext(ApiContext)
+    const [search, setSearch] = useState('')
+    const [filmsFilter, setFilmsFilter] = useState([])
+
+    useEffect(() => {
+        const filtered = films.filter(film => film.title.toLowerCase().includes(search.toLowerCase()))
+        setFilmsFilter(filtered)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [search])
 
     return(
         <>
@@ -10,13 +18,22 @@ export default function LayoutPage(){
             <div className="d-flex justify-content-between align-items-center">
                 <h1>Films</h1>
                 <div className="input-group mb-3">
-                    <input type="text" className="form-control" placeholder="search for your movie" aria-label="Recipient's username" aria-describedby="button-addon2"/>
-                    <button className="btn btn-outline-secondary" type="button" id="button-addon2">search</button>
+                    <input
+                     type="search"
+                     name="searchText" 
+                     id="searchText" 
+                     className="form-control" 
+                     placeholder="search for your movie" 
+                     aria-label="Recipient's username" 
+                     aria-describedby="button-addon2" 
+                     value={search} 
+                     onChange={e => setSearch(e.target.value)}/>
+                    <button className="btn btn-outline-secondary" type="submit" id="button-addon2">search</button>
                 </div>
             </div>
 
             <ul>
-                {films.map(film => (<li key={film.id}>
+                {filmsFilter.map(film => (<li key={film.id}>
                     {film.title}, {film.original_title}, {film.vote_average}, {film.original_language}
                 </li>))}
             </ul>
